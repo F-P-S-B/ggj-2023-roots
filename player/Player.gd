10a523e474d6957200c649e79db58b6cfe4b5006
Félix SB <felix.sassus-bourda@proton.me>
2023-02-04 15:36:20 +0100

    Refactor du script player

--- player/Player.gd
+++ player/Player.gd
@@ -1,18 +1,8 @@
 extends KinematicBody2D
 
-export var jump_speed := 1000
-export var air_jump_speed := 1000
-export var super_jump_speed := 2000
-export var dash_speed := 1000
-export var super_jump_charge = 200
-export var gravity := 100
-export var vert_friction := 0.025
-export var horiz_friction := 0.2
-export var max_speed := 350.0
-export var max_skill_count := 5
-var skill_count := 0
-var show_menu := false
-var skilltree
+"""
+Types
+"""
 enum Skills{
 	MOVE,
 	JUMP1,
@@ -25,6 +15,20 @@ enum Skills{
 	LANTERN
 }
 
+"""
+Config
+"""
+export var jump_speed := 1000
+export var air_jump_speed := 1000
+export var super_jump_speed := 2000
+export var dash_speed := 1000
+export var super_jump_charge = 200
+export var gravity := 100
+export var vert_friction := 0.025
+export var horiz_friction := 0.2
+export var max_speed := 350.0
+export var max_skill_count := 5
+
 export var enabled_skills := {
 	Skills.MOVE : false,
 	Skills.JUMP1 : false,
@@ -36,28 +40,51 @@ export var enabled_skills := {
 	Skills.RAISE_PLATFORM : false,
 	Skills.LANTERN : false,
 }
-func calculate_skill_count():
-	var count = 0
-	for skill in enabled_skills:
-		count += int(enabled_skills[skill])
-	return count
 
-func _ready():
-	skill_count = calculate_skill_count()
-	print("Skill count:", skill_count)
-	skilltree = get_node("Skilltree")
-	
-	
+"""
+Useful vars
+"""
 var velocity := Vector2.ZERO
 var direction := 1
 var air_jumps := 0
 var dashes := 0
 var dash_timer := 0
+var skill_count : int
+var show_menu := false
+var skilltree : CenterContainer
 
+"""
+Main functions
+"""
+func _ready():
+	skill_count = calculate_skill_count()
+	print("Skill count:", skill_count)
+	skilltree = get_node("Skilltree")
+	
+
+func _physics_process(_delta):
+	if toggle_menu():
+		return
+	if dash():
+		return
+	fall()
+	move()
+	jump()
+	super_jump()
+	wall_jump()
+	gliding()
+	raise_platform()
+	lantern()
+	velocity = move_and_slide(velocity, Vector2.UP)
+"""
+Movements and skills
+"""
 
 func fall():
 	velocity.y = (1-vert_friction)*velocity.y
 	velocity.y += gravity
+	if is_on_floor():
+		velocity.y = 1
 	
 func move():
 	if not (enabled_skills[Skills.MOVE]) and is_on_floor():
@@ -79,6 +106,12 @@ func move():
 
 
 func jump():
+	if not (enabled_skills[Skills.JUMP1] or enabled_skills[Skills.JUMP2]):
+		return
+	if (enabled_skills[Skills.JUMP1] and 
+		enabled_skills[Skills.JUMP2] and 
+		is_on_floor()):
+			air_jumps = 1
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			velocity.y = -jump_speed
@@ -88,71 +121,53 @@ func jump():
 				velocity.y = -air_jump_speed
 	
 func super_jump():
+	if not enabled_skills[Skills.SUPER_JUMP]:
+		return
 	pass
 	
 func wall_jump():
+	if not enabled_skills[Skills.WALL_JUMP]:
+		return
 	pass
 	
 func gliding():
+	if enabled_skills[Skills.GLIDING]:
+		return
 	pass
 	
 func dash():
+	if dash_timer > 0:
+		dash_timer -= 1
+		velocity.x = direction * dash_speed
+		velocity.y = 0
+		var _m = move_and_slide(velocity, Vector2.UP)
+		return true
+		
+	if not enabled_skills[Skills.DASH]:
+		return false
+		
+	if(is_on_floor()):
+		dashes = 1
 	if Input.is_action_just_pressed("dash"):
 		if(dashes >= 1):
 			dashes -= 1
 			dash_timer = 5
+		return true
+	return false
 	
 func raise_platform():
+	if not enabled_skills[Skills.RAISE_PLATFORM]:
+		return
 	pass
 	
 func lantern():
+	if not enabled_skills[Skills.LANTERN]:
+		return
 	pass
-	
-func _physics_process(_delta):
-	if Input.is_action_just_pressed("toggle_menu"):
-		show_menu = not show_menu
-	if show_menu:
-		skilltree.show()
-		return
-	skilltree.hide()
-	
-	if dash_timer > 0:
-		dash_timer -= 1
-		print("direction ",direction)
-		print("dash speed ", dash_speed)
-		velocity.x = direction * dash_speed
-		velocity.y = 0
-		move_and_slide(velocity, Vector2.UP)
-		return
-	
-	if(is_on_floor()):
-		velocity.y = 1
-	else:
-		fall()
-	move()
-	if enabled_skills[Skills.JUMP1] or enabled_skills[Skills.JUMP2]:
-		if enabled_skills[Skills.JUMP1] and enabled_skills[Skills.JUMP2]:
-			if (is_on_floor()):
-				air_jumps = 1
-		jump()
-	if enabled_skills[Skills.SUPER_JUMP]:
-		super_jump()
-	if enabled_skills[Skills.WALL_JUMP]:
-		wall_jump()
-	if enabled_skills[Skills.GLIDING]:
-		gliding()
-	if enabled_skills[Skills.DASH]:
-		if(is_on_floor()):
-			dashes = 1
-		dash()
-	if enabled_skills[Skills.RAISE_PLATFORM]:
-		raise_platform()
-	if enabled_skills[Skills.LANTERN]:
-		lantern()
-	velocity = move_and_slide(velocity, Vector2.UP)
 
-
-
+"""
+Signals
+"""
 func _on_Dash_Button_pressed():
 	enable_skill(Skills.DASH)
 
@@ -179,6 +194,25 @@ func _on_Glide_Button_pressed():
 
 func _on_Platform_Button_pressed():
 	enable_skill(Skills.RAISE_PLATFORM)
+
+
+"""
+Utilities
+"""
+func calculate_skill_count():
+	var count = 0
+	for skill in enabled_skills:
+		count += int(enabled_skills[skill])
+	return count
+
+func toggle_menu():
+	if Input.is_action_just_pressed("toggle_menu"):
+		show_menu = not show_menu
+	if show_menu:
+		skilltree.show()
+		return true
+	skilltree.hide()
+	return false
 
 func enable_skill(skill: int): # type: Enum Skills
 	if enabled_skills[skill]:
